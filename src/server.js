41b15166fe26c5bbd5c9084a071b882ac46cd60f
Léo Coelho <leosouza.coelho13@gmail.com>
2020-10-19 20:18:17 -0300

    Primeiro commit

---
+++ src/server.js
@@ -0,0 +1,25 @@
+
+
+const express = require('express')
+const server = express()
+
+const {pageLanding, pageStudy, pageGiveClasses, saveClasses} = require('./pages')
+
+
+//config nunjucks
+const nunjucks = require('nunjucks')
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true,
+})
+
+server
+.use(express.urlencoded({ extended: true}))
+//Configurar arquivos estáticos (css,scripts,imagnes)
+.use(express.static("public"))
+//rotas
+.get("/", pageLanding)
+.get("/study", pageStudy)
+.get("/give-classes", pageGiveClasses)
+.post("/save-classes", saveClasses)
+.listen(5500)
